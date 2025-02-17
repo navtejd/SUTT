a7710f45ad2c16102987db5a28032b34f8025c0d
Navtej Dua <navtej@MacBook-Pro-2.local>
2025-02-18 00:02:20 +0530

    comment added

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import json
 mess_menu_df = pd.read_excel("messmenu.xlsx", sheet_name="Sheet1")
 mess_menu_json = {}
 
-def format_cell_value(cell_value):
-    if isinstance(cell_value, str):
-        if set(cell_value.strip()) == {"*"}:
-            return ""
-        cell_value = " ".join(cell_value.split())
+def format_cell_value(cell_value): #formats the stuff we have , in the sense it removes cells with stars and nulls
+    if isinstance(cell_value, str): #isinstance means it checks if cellvalue is string
+        if set(cell_value.strip()) == {"*"}: #checks if cell contains only * values
+            return "" #empty string essentially skips it
+        cell_value = " ".join(cell_value.split()) #removes spaces
     return cell_value
 
 def daily_menu(df):
-    for col in range(df.shape[1]):
-        daily_menu = {"Breakfast": [], "Lunch": [], "Dinner": []}
-        date_str = df.iloc[0, col]
-        formatted_date = date_str.strftime("%d-%m-%Y")
+    for col in range(df.shape[1]): #basically forloop going thru all the columns
+        daily_menu = {"Breakfast": [], "Lunch": [], "Dinner": []} #dictionary for the todays menu, with seperate lists
+                                                                  #for eachcourse
+        date_str = df.iloc[0, col] #extracts date from first row of the column
+        formatted_date = date_str.strftime("%d-%m-%Y")  #formats the date
 
-        for item in range(2, 30):
+        for item in range(2, 30): #for loop to iterate thru daily items
             cell_value = df.iloc[item, col]
             cell_value = format_cell_value(cell_value)
-            if pd.isna(cell_value) or cell_value == "":
+            if pd.isna(cell_value) or cell_value == "": #skips cells where there is a blankspace or null or stars
                 continue
-
+          #BELOW are lists for Each courses, items getting added as the loop iterates
             if 2 <= item <= 10:
                 daily_menu["Breakfast"].append(cell_value)
             elif 13 <= item <= 20:
@@ -38,5 +39,5 @@ def export_json(json_file):
     with open("mess_menu.json", "w") as json_file:
         json.dump(mess_menu_json, json_file, indent=4)
 
-daily_menu(mess_menu_df)
-export_json(mess_menu_json)
+daily_menu(mess_menu_df) #creating parsed menu
+export_json(mess_menu_json) #json file generation
